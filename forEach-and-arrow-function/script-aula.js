94fb927ea922b26e427102c15e3807d5b162a982
Caio Kenedy <caiokenedysantos@gmail.com>
2022-03-15 16:27:31 -0300

    exercising foreach and arrow functions

---
+++ forEach-and-arrow-function/script-aula.js
@@ -0,0 +1,50 @@
+<<<<<<< HEAD
+// const imgs = document.querySelectorAll("img");
+
+//  imgs.forEach(function (item, index, array) {
+//    console.log(item, index, array);
+//  });
+
+const titulos = document.getElementsByClassName("titulo");
+const titulosArray = Array.from(titulos);
+
+titulosArray.forEach(function (item) {
+  console.log(item);
+});
+
+const imgs = document.querySelectorAll("img");
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+imgs.forEach((item) => console.log(item));
+=======
+const animais = document.getElementById("animais");
+
+const gridSection = document.getElementsByClassName("grid-section");
+
+const primeiraLi = document.querySelector("li");
+
+const primeiraUl = document.querySelector("ul");
+
+const linksInternos = document.querySelector("[href^='#']");
+
+const animaisImg = document.querySelectorAll(".animais img");
+console.log(animaisImg);
+
+const gridSectionHTML = document.getElementsByClassName("grid-section");
+
+const gridSectionNode = document.querySelectorAll(".grid-section");
+
+primeiraUl.classList.add("grid-section");
+
+// console.log(gridSectionHTML);
+// console.log(gridSectionNode);
+
+gridSectionNode.forEach(function (item, index) {
+  console.log(item);
+});
+
+const arrayGrid = Array.from(gridSectionHTML);
+>>>>>>> 707e8bee44b68bcfb498b6e1dc7f709a60128535
---
+++ forEach-and-arrow-function/script.js
@@ -0,0 +1,43 @@
+<<<<<<< HEAD
+// Mostre no console cada parágrado do site
+const paragrafos = document.querySelectorAll("p");
+
+paragrafos.forEach((item) => {
+  console.log(item);
+});
+// Mostre o texto dos parágrafos no console
+paragrafos.forEach((item) => {
+  console.log(item.innerText);
+});
+// Como corrigir os erros abaixo:
+const imgs = document.querySelectorAll("img");
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(() => {
+  console.log(i++);
+});
+
+imgs.forEach(() => i++);
+=======
+// Retorne no console todas as imagens do site
+const allImg = document.querySelectorAll("img");
+console.log(allImg);
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const sixImg = document.querySelectorAll("[src^='./img/imagem']");
+console.log(sixImg);
+// Selecione todos os links internos (onde o href começa com #)
+const linksInternos = document.querySelectorAll("[href^='#']");
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+
+const primeiroH2 = document.querySelector(".animais-descricao h2");
+console.log(primeiroH2);
+
+// Selecione o último p do site
+const paragrafos = document.querySelectorAll("p");
+console.log(paragrafos[paragrafos.length - 1]);
+>>>>>>> 707e8bee44b68bcfb498b6e1dc7f709a60128535
