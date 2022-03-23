278c02a1a93c02e6173f5e4ce7930347845f0ce6
Caio Kenedy <caiokenedysantos@gmail.com>
2022-03-23 12:36:42 -0300

    Native constructor

--- Prototype/script.js
+++ Prototype/script.js
@@ -13,3 +13,20 @@ Pessoa.prototype.cagar = function () {
 
 const caio = new Pessoa("Caio", 26);
 console.log(Pessoa.prototype);
+
+const pais = "Brasil";
+const cidade = new String("Rio");
+
+const listaAnimais = ["Cachorro", "Gato", "Peixe", "Passaro"];
+
+const lista = document.querySelectorAll("li");
+const listaArray = Array.prototype.slice.call(lista);
+const listaArrayNova = Array.from(lista);
+
+const Carro = {
+  marca: "Ford",
+  preco: 2000,
+  acelerar() {
+    return true;
+  },
+};
