81fa6ff5bbf2734a894dff27b50a62775b591d38
Caio Kenedy <caiokenedysantos@gmail.com>
2022-03-23 14:40:18 -0300

    exercises

--- Native-host-user/script-aula1.js
+++ Native-host-user/script-aula1.js
@@ -1,32 +1,7 @@
-function Pessoa(nome, idade) {
-  this.nome = nome;
-  this.idade = idade;
+console.log(typeof Array.from !== "undefined");
+
+if (typeof Array.from !== "undefined") {
+  console.log("existe");
+} else {
+  console.log("nao existe");
 }
-
-Pessoa.prototype.andar = function () {
-  return this.nome + " andou";
-};
-
-Pessoa.prototype.cagar = function () {
-  return this.nome + " cagou";
-};
-
-const caio = new Pessoa("Caio", 26);
-console.log(Pessoa.prototype);
-
-const pais = "Brasil";
-const cidade = new String("Rio");
-
-const listaAnimais = ["Cachorro", "Gato", "Peixe", "Passaro"];
-
-const lista = document.querySelectorAll("li");
-const listaArray = Array.prototype.slice.call(lista);
-const listaArrayNova = Array.from(lista);
-
-const Carro = {
-  marca: "Ford",
-  preco: 2000,
-  acelerar() {
-    return true;
-  },
-};
--- Native-host-user/script.js
+++ Native-host-user/script.js
@@ -1,38 +1,18 @@
-// Crie uma função construtora de Pessoas
-// Deve conter nome, sobrenome e idade
-// Crie um método no protótipo que retorne
-// o nome completo da pessoa
+// Liste 5 objetos nativos
+Object;
+String;
+Array;
+Function;
+Number;
 
-function Pessoas(nome, sobrenome, idade) {
-  this.nome = nome;
-  this.sobrenome = sobrenome;
-  this.idade = idade;
-}
+// Liste 5 objetos do browser
+Window;
+history;
+Document;
+HTMLCollection;
+NodeList;
 
-Pessoas.prototype.fullName = function () {
-  return `${this.nome} ${this.sobrenome}`;
-};
-
-const caio = new Pessoas("Caio", "Kenedy", 26);
-
-// Liste os métodos acessados por
-// dados criados com NodeList,
-// HTMLCollection, Document
-
-// NodeList / forEach, item, entries, etc
-// HTMLCollection / item, length, namedItem
-// Document / possui muitos só checar o .prototype ou Object.getOwnPropertyNames(NodeList.prototype)
-
-// Liste os construtores dos dados abaixo
-const li = document.querySelector("li");
-
-li; // HTMLLIELEMTENT
-li.click; // FUNCTION
-li.innerText; // STRING
-li.value; // NUMBER
-li.hidden; // BOOLEAN
-li.offsetLeft; // NUMBER
-li.click(); // UNDEFINED
-
-// Qual o construtor do dado abaixo:
-li.hidden.constructor.name; //String
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+webkitURL;
+WebKitCSSMatrix;
