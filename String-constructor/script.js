6f9b9055175aedb461db621f3c21d8456c7d8b32
Caio Kenedy <caiokenedysantos@gmail.com>
2022-03-29 15:57:20 -0300

    string constructor in real life problems

--- String-constructor/script.js
+++ String-constructor/script.js
@@ -27,3 +27,15 @@ const listaPrecos = ["R$ 99", "R$ 199", "R$ 12000"];
 listaPrecos.forEach((preco) => {
   console.log(preco.padStart(10, "."));
 });
+
+let listaItens = "Camisas Bonés Calças Bermudas Vestidos Saias";
+
+listaItens = listaItens.replace(/[ ]+/g, ", ");
+console.log(listaItens);
+
+const arrayLista = listaItens.split(", ");
+console.log(arrayLista);
+
+const htmlText = "<div>O melhor item</div><div>A melhor lista</div>";
+const htmlArray = htmlText.split("div");
+const novoHtml = htmlArray.join("section");
