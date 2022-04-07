6f5d8c8aca39c8945ad2dee27bdcd2a75420c0f6
Caio Kenedy <caiokenedysantos@gmail.com>
2022-04-07 14:44:03 -0300

    Object exercises

--- Object/script.js
+++ Object/script.js
@@ -1,9 +1,21 @@
 // Crie uma função que verifique
 // corretamente o tipo de dado
+function check(dado) {
+  return Object.prototype.toString.call(dado);
+}
 
+const car = "Ronaldo";
 // Crie um objeto quadrado com
 // a propriedade lados e torne
 // ela imutável
+
+const square = {};
+Object.defineProperties(square, {
+  sides: {
+    value: 4,
+    enumerable: false,
+  },
+});
 
 // Previna qualquer mudança
 // no objeto abaixo
@@ -13,6 +25,12 @@ const configuracao = {
   background: "#333",
 };
 
+Object.freeze(configuracao);
+Object.seal(configuracao);
+Object.preventExtensions(configuracao);
 // Liste o nome de todas
 // as propriedades do
 // protótipo de String e Array
+
+Object.getOwnPropertyNames(String);
+Object.getOwnPropertyNames(Array);
