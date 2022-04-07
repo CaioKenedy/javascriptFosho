7284be3a5a5753e5193eb61cc4cb2403637fd6a5
Caio Kenedy <caiokenedysantos@gmail.com>
2022-04-07 14:26:57 -0300

    exercises initial commit

--- Object/script.js
+++ Object/script.js
@@ -1,74 +1,18 @@
-const person = new Object({
-  name: "Caio",
-  age: 26,
-});
+// Crie uma função que verifique
+// corretamente o tipo de dado
 
-const car = {
-  wheels: 4,
-  init(value) {
-    this.brand = value;
-    return this;
-  },
-  year: 2020,
-  speed() {
-    return this.brand + " speed up";
-  },
-  honk() {
-    return this.brand + " honk";
-  },
+// Crie um objeto quadrado com
+// a propriedade lados e torne
+// ela imutável
+
+// Previna qualquer mudança
+// no objeto abaixo
+const configuracao = {
+  width: 800,
+  height: 600,
+  background: "#333",
 };
 
-const camaro = Object.create(car);
-camaro.init("Camaro");
-
-const ferrari = Object.create(car).init("Ferrari");
-
-const autoFunction = {
-  speedUp() {
-    return "speed up";
-  },
-  honk() {
-    return "honk";
-  },
-};
-
-const bike = {
-  wheels: 2,
-  helmet: true,
-};
-
-const auto = {
-  wheels: 4,
-  trunk: true,
-};
-
-Object.assign(bike, autoFunction);
-
-// Object.defineProperties(bike, {
-//   tankCapacity: {
-//     value: 30,
-//     enumerable: false,
-//   },
-// });
-
-Object.defineProperties(bike, {
-  tankCapacity: {
-    get() {
-      return this.capacity;
-    },
-    set(valor) {
-      return (this.capacity = valor * 4 + " L");
-    },
-  },
-});
-
-Object.getOwnPropertyDescriptors(Array);
-Object.getOwnPropertyDescriptors(Array.prototype);
-Object.getOwnPropertyDescriptor(window, "innerHeight");
-Object.getOwnPropertyNames(Array);
-
-const fruit = ["Banana"];
-
-Object.getPrototypeOf(fruit);
-
-Object.prototype.toString.call(); // verifica o tipo de dado
+// Liste o nome de todas
+// as propriedades do
+// protótipo de String e Array
