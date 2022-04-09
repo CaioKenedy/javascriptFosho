e43387652a4b7d4a79341d564751b371b4419df6
Caio Kenedy <caiokenedysantos@gmail.com>
2022-04-09 15:04:12 -0300

    working on modal js

--- DOM-EFFECTS/modules/js/script.js
+++ DOM-EFFECTS/modules/js/script.js
@@ -2,6 +2,7 @@ import initScrollSmooth from "../js/modules/smooth-scroll.js";
 import initAnimacaoScroll from "../js/modules/scroll-animation.js";
 import initAccordion from "../js/modules/accordion.js";
 import initTabNav from "../js/modules/tab-nav.js";
+import initModal from "../js/modules/modal.js";
 
 initScrollSmooth();
 initAnimacaoScroll();
