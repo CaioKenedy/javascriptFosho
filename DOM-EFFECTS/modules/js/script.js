de10b0991bf7bbea3d642af5b26c4362aad0ef87
Caio Kenedy <caiokenedysantos@gmail.com>
2022-04-09 12:43:56 -0300

    created modules for js archives

---
+++ DOM-EFFECTS/modules/js/script.js
@@ -0,0 +1,9 @@
+import initScrollSmooth from "../js/modules/smooth-scroll.js";
+import initAnimacaoScroll from "../js/modules/scroll-animation.js";
+import initAccordion from "../js/modules/accordion.js";
+import initTabNav from "../js/modules/tab-nav.js";
+
+initScrollSmooth();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
