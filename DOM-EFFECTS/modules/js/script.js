8e6a3b66f598fc86d68d153abab694d59c192126
Caio Kenedy <caiokenedysantos@gmail.com>
2022-04-11 11:25:36 -0300

    tooltip initial commit

--- DOM-EFFECTS/modules/js/script.js
+++ DOM-EFFECTS/modules/js/script.js
@@ -3,6 +3,7 @@ import initAnimacaoScroll from "../js/modules/scroll-animation.js";
 import initAccordion from "../js/modules/accordion.js";
 import initTabNav from "../js/modules/tab-nav.js";
 import initModal from "../js/modules/modal.js";
+import initToolTip from "../js/modules/tooltip.js";
 
 initScrollSmooth();
 initAnimacaoScroll();
