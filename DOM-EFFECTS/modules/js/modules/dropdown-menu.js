ee2c0a9aeb62e681090ad40be2afeb9d89b08377
Caio Kenedy <caiokenedysantos@gmail.com>
2022-04-11 13:44:25 -0300

    event bubble

--- DOM-EFFECTS/modules/js/modules/dropdown-menu.js
+++ DOM-EFFECTS/modules/js/modules/dropdown-menu.js
@@ -1,1 +1,14 @@
 export default function initDropDownMenu() {}
+
+const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+
+dropdownMenus.forEach((menu) => {
+  ["touchstart", "click"].forEach((userEvent) => {
+    menu.addEventListener(userEvent, handleClick);
+  });
+});
+
+function handleClick(event) {
+  event.preventDefault();
+  this.classList.toggle("active");
+}
