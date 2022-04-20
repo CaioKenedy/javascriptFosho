e7edeb8d74364ba6d049ee8164d2519ac2c582ad
Caio Kenedy <caiokenedysantos@gmail.com>
2022-04-20 13:02:12 -0300

    working on numbers

--- DOM-EFFECTS/modules/js/modules/anime-numbers.js
+++ DOM-EFFECTS/modules/js/modules/anime-numbers.js
@@ -1,3 +1,24 @@
 export default function initAnimeNumbers() {}
 
 const numbers = document.querySelectorAll("[data-numero]");
+
+numbers.forEach((number) => {
+  const total = +number.innerText;
+  const increment = Math.floor(total / 100);
+  let start = 0;
+  const timer = setInterval(() => {
+    start = start + increment;
+    number.innerText = start;
+    if (start > total) {
+      number.innerText = total;
+      clearInterval(timer);
+    }
+  }, 25 * Math.random());
+});
+
+function handleMutation() {}
+
+const observerTarget = document.querySelector(".numeros");
+const observer = new MutationObserver(handleMutation);
+
+observer.observe(observerTarget, { attributes: true });
