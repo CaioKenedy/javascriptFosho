a5dfd1a4e4332b4bd445fd6557997162345a8ca0
Caio Kenedy <caiokenedysantos@gmail.com>
2022-04-23 10:44:42 -0300

    added Open/Close UI

--- DOM-EFFECTS/modules/js/modules/date.js
+++ DOM-EFFECTS/modules/js/modules/date.js
@@ -1,5 +1,17 @@
 export default function initDate() {}
 
-const now = new Date();
+const open = document.querySelector("[data-semana]");
+const daysWeek = open.dataset.semana.split(",").map(Number);
+const hourWeek = open.dataset.horario.split(",").map(Number);
 
-console.log(now.getDate());
+const dateNow = new Date();
+const dayOfWeek = dateNow.getDay();
+const hourNow = dateNow.getHours();
+
+const openToday = daysWeek.indexOf(dayOfWeek) !== -1;
+
+const openHour = hourNow >= hourWeek[0] && hourNow < hourWeek[1];
+
+if (openToday && openHour) {
+  open.classList.add("aberto");
+}
