770691261298012cdb6988a8783b75da3d6fa603
Caio Kenedy <caiokenedysantos@gmail.com>
2022-04-26 12:23:47 -0300

    more exercises

--- Promises/script.js
+++ Promises/script.js
@@ -30,7 +30,35 @@ const promise = new Promise((resolve, reject) => {
   }
 });
 
-const retorno = promise.then((resolucao) => {
-  resolucao.profissao = "tecnico";
-  return resolucao;
+const retorno = promise
+  .then((resolucao) => {
+    resolucao.profissao = "tecnico";
+    return resolucao;
+  })
+  .then((resolucao) => {
+    console.log(resolucao);
+  })
+  .catch((rejeitada) => {
+    console.log(rejeitada);
+  })
+  .finally(() => {
+    console.log("acabou");
+  });
+
+const login = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("Login Efetuado");
+  }, 1000);
+});
+
+const dados = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("Dados Carregados");
+  }, 1500);
+});
+
+const tudoCarregado = Promise.all([login, dados]);
+
+tudoCarregado.then((respostas) => {
+  console.log(respostas);
 });
