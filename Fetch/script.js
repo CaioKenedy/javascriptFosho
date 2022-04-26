ac48d688a48702c1ef5fff4c8a578160a3164d3e
Caio Kenedy <caiokenedysantos@gmail.com>
2022-04-26 14:46:17 -0300

    more fetch examples

--- Fetch/script.js
+++ Fetch/script.js
@@ -1,11 +1,27 @@
-const doc = fetch("https://viacep.com.br/ws/41950350/json/");
+// const doc = fetch("https://viacep.com.br/ws/41950350/json/");
 
-doc
+// doc
+//   .then((resolucao) => {
+//     return resolucao.json();
+//   })
+//   .then((body) => {
+//     const conteudo = document.querySelector(".conteudo");
+//     conteudo.innerText = body.logradouro;
+//     console.log(body);
+//   });
+
+const sobre = fetch("./sobre.html");
+
+const div = document.createElement("div");
+
+sobre
   .then((resolucao) => {
-    return resolucao.json();
+    return resolucao.text();
   })
   .then((body) => {
-    const conteudo = document.querySelector(".conteudo");
-    conteudo.innerText = body.logradouro;
-    console.log(body);
+    div.innerHTML = body;
+    const titulo = div.querySelector("h1");
+    document.querySelector("h1").innerText = titulo.innerText;
+
+    console.log(titulo);
   });
