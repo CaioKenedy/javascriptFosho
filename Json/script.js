6c75870f63d9823b9501dd23e36fa0a71e4af43b
Caio Kenedy <caiokenedysantos@gmail.com>
2022-04-27 13:33:12 -0300

    JSON exercises

--- Json/script.js
+++ Json/script.js
@@ -8,3 +8,23 @@ fetch("./dados.json")
       console.log(coisasDentro.aula);
     });
   });
+
+//.json faz a mesma coisa que parse faz.... JSON.parse(stringaqui)
+
+//mandar arquivo para JSON
+
+const configuracoes = {
+  player: "Google",
+  tempo: 25.5,
+  aula: "2.1 Javascript",
+};
+
+const stringConfig = JSON.stringify(configuracoes);
+
+console.log(stringConfig);
+
+// exemplo real
+
+localStorage.config = JSON.stringify(configuracoes);
+
+console.log(JSON.parse(localStorage.config));
