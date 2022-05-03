00715d86076db997d1a128d51f66451df8c03ce7
Caio Kenedy <caiokenedysantos@gmail.com>
2022-05-03 11:37:37 -0300

    class vs constructor function

--- Classes/script.js
+++ Classes/script.js
@@ -13,3 +13,39 @@ Button.prototype.element = function () {
 const blueButton = new Button("Comprar", "Blue");
 
 console.log(blueButton.element());
+
+// ------ Usando Class para substituir constructor function
+
+class Button2 {
+  constructor(text, background) {
+    this.text = text;
+    this.background = background;
+  }
+  // o nome do callback fica a seu critério, utilizei element.
+  element() {
+    const buttonElement2 = document.createElement("button");
+    buttonElement2.innerText = this.text;
+    buttonElement2.style.background = this.background;
+    return buttonElement2;
+  }
+}
+
+const redButton = new Button2("Comprar", "Red");
+
+console.log(redButton.element());
+
+// Diferença entre Class e Constructor Function
+
+class NomeDaClasse {
+  constructor(propriedade) {
+    this.propriedade = propriedade;
+  }
+  metodo1() {}
+  metodo2() {}
+}
+
+function NomeDaFuncao(propriedade) {
+  this.propriedade = propriedade;
+}
+NomeDaFuncao.prototype.metodo1 = function () {};
+NomeDaFuncao.prototype.metodo2 = function () {};
