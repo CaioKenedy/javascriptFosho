68662066ba4a85b96481c0efeca0eedc6ede250d
Caio Kenedy <caiokenedysantos@gmail.com>
2022-05-03 14:17:06 -0300

    extended examples

--- Extends/script.js
+++ Extends/script.js
@@ -1,48 +1,30 @@
-const button = {
-  get tamanho() {
-    return this.numero || 100;
-  },
-  set tamanho(numero) {
-    this.numero = numero * 3;
-  },
-};
-
-// definindo valor estatico
-
-const matematica = {
-  get PI() {
-    return 3.14;
-  },
-};
-
-// Set para modificar propriedades
-
-const frutas = {
-  lista: [],
-  set nova(fruta) {
-    this.lista.push(fruta);
-  },
-};
-
-// executando Set
-frutas.nova = "Banana";
-frutas.nova = "Morango";
-
-class Button2 {
-  constructor(text, color) {
-    this.text = text;
-    this.color = color;
+class Veiculo {
+  constructor(rodas) {
+    this.rodas = rodas;
   }
-  get element() {
-    const type = this.elementType || "button";
-    const buttonElement = document.createElement(type);
-    buttonElement.innerText = this.text;
-    buttonElement.style.color = this.color;
-    return buttonElement;
-  }
-  set element(type) {
-    this.this.elementType = type;
+  acelerar() {
+    console.log("Acelerou");
   }
 }
 
-const blueButton = new Button2("Comprar", "blue");
+// estende um objeto generico para um mais especifico
+
+class Moto extends Veiculo {
+  //posso usar o super para extender o construtor
+  constructor(rodas, capacete) {
+    super(rodas);
+    this.capacete = capacete;
+  }
+  // também posso usar a palavra Super, para acessar um metódo herdado. exemplo super.acelerar()
+  testar() {
+    super.acelerar();
+  }
+  empinar() {
+    console.log("Empinou com " + this.rodas + " rodas");
+  }
+  //também posso sobre escrever um metodo, exemplo, posso por o acelerar() da classe anterior aqui.
+}
+
+const honda = new Moto(2, true);
+honda.empinar();
+honda.testar();
